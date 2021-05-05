acd2fca2ead8cd2497cc1f4be08538b99496d263
worldyone <amanohikari142857@gmail.com>
2021-05-05 19:46:55 +0900

    クイズ一覧の改装

--- src/Show.tsx
+++ src/Show.tsx
@@ -1,28 +1,139 @@
-import { Button, Card, List, makeStyles } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+  withStyles,
+} from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableFooter from "@material-ui/core/TableFooter";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 import { db } from "./firebase/firebase";
+import { Button, TableHead } from "@material-ui/core";
 import HomeIcon from "@material-ui/icons/Home";
+import styles from "./styles/Show.module.css";
 
-const useStyles = makeStyles({
-  list: {
-    margin: "auto",
-    width: "40%",
-  },
-  card: {
-    margin: "auto",
-    width: "100%",
-    border: "solid",
-    borderColor: "dimgray",
-    marginTop: "10px",
-    marginBottom: "10px",
-    cursor: "pointer",
+const useStyles1 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexShrink: 0,
+      marginLeft: theme.spacing(2.5),
+    },
+  })
+);
+
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onChangePage: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number
+  ) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 500,
   },
 });
 
 const Show: React.FC = (props: any) => {
   const [titles, setTitles] = useState([{ id: "", sentence: "" }]);
-
-  const classes = useStyles();
 
   useEffect(() => {
     const unSub = db.collection("quizzes").onSnapshot((snapshot) => {
@@ -36,27 +147,86 @@ const Show: React.FC = (props: any) => {
     return () => unSub();
   }, []);
 
+  const classes = useStyles2();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, titles.length - page * rowsPerPage);
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <div>
-      <List className={classes.list}>
-        {titles.map((title, index) => (
-          <Card
-            className={classes.card}
-            variant="outlined"
-            onClick={async () => {
-              try {
-                props.history.push("/solve/" + title.id);
-              } catch (error) {
-                alert(error.message);
-              }
-            }}
-          >
-            <h3>問題番号 {index + 1}</h3>
-            {title.sentence}
-          </Card>
-        ))}
-      </List>
-
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="custom pagination table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>問題番号</StyledTableCell>
+              <StyledTableCell align="left">タイトル</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? titles.slice(
+                  page * rowsPerPage,
+                  page * rowsPerPage + rowsPerPage
+                )
+              : titles
+            ).map((title, index) => (
+              <TableRow key={title.id}>
+                <TableCell component="th" scope="row" style={{ width: 120 }}>
+                  {index + 1}
+                </TableCell>
+                <TableCell
+                  className={styles.Water}
+                  style={{ width: 1000, cursor: "pointer" }}
+                  align="left"
+                  onClick={async () => {
+                    try {
+                      props.history.push("/solve/" + title.id);
+                    } catch (error) {
+                      alert(error.message);
+                    }
+                  }}
+                >
+                  <div className={styles.WaterText}>{title.sentence}</div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={titles.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true,
+                }}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
       <br />
       <br />
       <Button
